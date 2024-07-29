264559b0274c04830741ef12d83149d07e33eabc
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-07-29 09:32:58 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
